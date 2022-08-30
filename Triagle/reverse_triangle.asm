2eb7d95d4ba9b50b3886e3af095f91697ea847e2
홍성민 <seanhong2000@gmail.com>
2022-08-31 03:24:50 +0900

    add reverse pyramid.

---
+++ Triagle/reverse_triangle.asm
@@ -0,0 +1,33 @@
+global    start
+          section   .text
+start:
+          mov       rdx, output             ; rdx holds address of next byte to write
+          mov       r8, 10                  ; initial line length
+          mov       r9, 0                   ; number of stars written on line so far
+line:
+          mov       byte [rdx], '*'         ; write single star
+          inc       rdx                     ; advance pointer to next cell to write
+          inc       r9                      ; "count" number so far on line
+          cmp       r9, r8                  ; did we reach the number of stars for this line?
+          jne       line                    ; not yet, keep writing on this line
+lineDone:
+          mov       byte [rdx], 10          ; write a new line char
+          inc       rdx                     ; and move pointer to where next char goes
+          dec       r8                      ; next line will be one char longer
+          mov       r9, 0                   ; reset count of stars written on this line
+          cmp       r8, maxlines            ; wait, did we already finish the last line?
+          jnl       line                    ; if not, begin writing this line
+done:
+          mov       rax, 0x02000004         ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          mov       rsi, output             ; address of string to output
+          mov       rdx, dataSize           ; number of bytes
+          syscall                           ; invoke operating system to do the write
+          mov       rax, 0x02000001         ; system call for exit
+          xor       rdi, rdi                ; exit code 0
+          syscall                           ; invoke operating system to exit
+
+          section   .bss
+maxlines  equ       1
+dataSize  equ       65
+output:   resb      dataSize
